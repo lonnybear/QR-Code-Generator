b7fe5615419351ea99fa4c0197e6c2da85e4c430
Lonny Le <lonnyorlonnie@gmail.com>
2024-04-15 17:09:26 -0700

    updated html/css/js

---
+++ index.js
@@ -0,0 +1,7 @@
+const download = document.querySelector('.download');
+const dark = document.querySelector('.dark');
+const light = document.querySelector('.light');
+const qrContainer = document.querySelector('#qrCode');
+const qrText = document.querySelector('.qr-text');
+const shareBtn = document.querySelector('.share-btn');
+const sizes = document.querySelector('.sizes');
